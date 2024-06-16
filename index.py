211ef33d9cfb48c30cb88e50156c700bb7d5351b
ananito <anisfhomsi@gmail.com>
2024-06-16 20:43:02 +0300

    rename test.py to index.py

---
+++ index.py
@@ -0,0 +1,13 @@
+from autorevanced.util import autorevanced
+
+
+# files = get_files()
+# print(files)
+# apk = get_apk("youtube")
+# print(apk)
+# file = build(apk, files)
+#
+# print(file[0])
+# print(f"file location: {file[1]}")
+
+print(autorevanced())
